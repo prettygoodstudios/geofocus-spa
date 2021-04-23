77ffbf0ae0d563f5ecdeb81b4aa55ca03dc1ae1d
Miguel Rust <hava12soft@gmail.com>
2021-04-23 10:45:27 -0600

    Setup initial webpack configuration for this project

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log("Did webpack load this")
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: 'dist/'
+  },
+  module: {
+    rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['@babel/preset-env'],
+                  plugins: ['@babel/plugin-proposal-class-properties']
+              }
+          }
+          },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+ 
+            'file-loader',
+ 
+          ],
+        },
+        { 
+        test: /\.m?tsx?$/, 
+          loader: "ts-loader" 
+        },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+};
