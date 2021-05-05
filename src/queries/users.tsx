7183a530a04dc268c38a12bac7b5181628693a45
Miguel Rust <hava12soft@gmail.com>
2021-05-05 16:05:35 -0600

    Refactored user queries to use fragment.

--- src/queries/users.tsx
+++ src/queries/users.tsx
@@ -1,165 +1,76 @@
 import {gql} from "@apollo/client";
 
-export const GET_TOP_USERS = gql`
-    query GetUsers {
-        topUsers {
-            profile_url,
-            display,
-            slug,
+export const USER_FRAGMENT = gql`
+    fragment PublicProfile on Profile {
+        profile_url,
+        display,
+        slug,
+        width,
+        height,
+        offsetX,
+        offsetY,
+        zoom,
+        photos {
+            caption,
+            url,
             width,
             height,
+            zoom,
             offsetX,
             offsetY,
-            zoom,
-            photos {
-                caption,
-                url,
+            slug,
+            views,
+            user {
+                profile_url,
+                display,
+                slug,
                 width,
                 height,
-                zoom,
                 offsetX,
                 offsetY,
-                slug,
-                views,
-                user {
-                    profile_url,
-                    display,
-                    slug,
-                    width,
-                    height,
-                    offsetX,
-                    offsetY,
-                    zoom
-                },
-                location {
-                    title,
-                    slug
-                }
+                zoom
+            },
+            location {
+                title,
+                slug
             }
+        }
+    }
+`;
+
+export const GET_TOP_USERS = gql`
+    ${USER_FRAGMENT}
+    query GetUsers {
+        topUsers {
+            ...PublicProfile   
         }
     }
 `;
 
 export const GET_USER = (slug: string) => gql`
+    ${USER_FRAGMENT}
     query GetUser {
         user(slug:"${slug}"){
-            display,
-            bio,
-            profile_url,
-            zoom,
-            width,
-            height,
-            offsetX,
-            offsetY,
-            slug,
-            photos{
-                views,
-                width,
-                height,
-                zoom,
-                caption,
-                url,
-                slug,
-                offsetX,
-                offsetY,
-                location {
-                    title,
-                    slug
-                },
-                user {
-                    profile_url,
-                    display,
-                    slug,
-                    width,
-                    height,
-                    offsetX,
-                    offsetY,
-                    zoom
-                }
-            }
+            ...PublicProfile   
         }
     }
 `;
 
 export const LOGIN = gql`
+    ${USER_FRAGMENT}
     mutation Login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
-            display,
-            bio,
-            profile_url,
-            zoom,
-            width,
-            height,
-            offsetX,
-            offsetY,
-            slug,
-            photos{
-                views,
-                width,
-                height,
-                zoom,
-                caption,
-                url,
-                slug,
-                offsetX,
-                offsetY,
-                location {
-                    title,
-                    slug
-                },
-                user {
-                    profile_url,
-                    display,
-                    slug,
-                    width,
-                    height,
-                    offsetX,
-                    offsetY,
-                    zoom
-                }
-            }
+            ...PublicProfile
         }
     }
 `;
 
 
 export const ME = gql`
+    ${USER_FRAGMENT}
     query me {
         me {
-            display,
-            bio,
-            profile_url,
-            zoom,
-            width,
-            height,
-            offsetX,
-            offsetY,
-            slug,
-            photos{
-                views,
-                width,
-                height,
-                zoom,
-                caption,
-                url,
-                slug,
-                offsetX,
-                offsetY,
-                location {
-                    title,
-                    slug
-                },
-                user {
-                    profile_url,
-                    display,
-                    slug,
-                    width,
-                    height,
-                    offsetX,
-                    offsetY,
-                    zoom
-                }
-            }
+            ...PublicProfile   
         }
     }
 `;
@@ -171,42 +82,10 @@ export const LOGOUT = gql`
 `;
 
 export const REGISTER = gql`
+    ${USER_FRAGMENT}
     mutation register($display: String, $email: String, $password: String, $bio: String, $file: Upload!, $offsetX: Float, $offsetY: Float $width: Float, $height: Float, $zoom: Float) {
         register(display: $display, email: $email, password: $password, bio: $bio, file: $file, offsetX: $offsetX, offsetY: $offsetY, width: $width, height: $height, zoom: $zoom) {
-            display,
-            bio,
-            profile_url,
-            zoom,
-            width,
-            height,
-            offsetX,
-            offsetY,
-            slug,
-            photos{
-                views,
-                width,
-                height,
-                zoom,
-                caption,
-                url,
-                slug,
-                offsetX,
-                offsetY,
-                location {
-                    title,
-                    slug
-                },
-                user {
-                    profile_url,
-                    display,
-                    slug,
-                    width,
-                    height,
-                    offsetX,
-                    offsetY,
-                    zoom
-                }
-            }
+            ...PublicProfile
         }
     }
 `;
