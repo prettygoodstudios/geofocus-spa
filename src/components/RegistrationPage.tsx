36c8d569402aeead509b0497c290e2edde9de573
Miguel Rust <hava12soft@gmail.com>
2021-08-09 18:03:48 -0600

    Built out profile form that is reusable

--- src/components/RegistrationPage.tsx
+++ src/components/RegistrationPage.tsx
@@ -1,146 +1,25 @@
 import { useMutation } from "@apollo/client";
-import { useTheme } from "@material-ui/core/styles";
-import { ReactElement, useReducer, useContext, useMemo } from "react";
-import { Redirect } from "react-router";
-import errorParser from "../helpers/errorParser";
+import { ReactElement, useContext } from "react";
 import { SET_USER } from "../helpers/Reducer";
 import { UserContext } from "../helpers/UserContext";
 import { REGISTER } from "../queries/users";
-import useButtons from "../styles/buttons";
-import useStandard from "../styles/standard";
-import Form, {FormInput} from "../widgets/Form";
-import PhotoUploader from "../widgets/PhotoUploader";
+import ProfileForm from "../widgets/ProfileForm";
 
 const RegistrationPage = () : ReactElement => {
-    const SET_INPUT = 'SET_INPUT';
-    const UPDATE_UPLOADER = 'UPDATE_UPLOADER';
-    const SET_ERROR = 'SET_ERROR';
-    const SET_COMPLETED = 'SET_COMPLETED';
 
     const context = useContext(UserContext);
-    const theme = useTheme();
-    const buttons = useButtons(theme)();
-    const standard = useStandard(theme);
-
-    const reducer = (state: any, {type, payload}: {type: string, payload: any}) => {
-        switch(type){
-            case SET_INPUT:
-                const {inputs} = state;
-                inputs[payload[0]].value = payload[1];
-                return {
-                    ...state,
-                    inputs
-                }
-            case UPDATE_UPLOADER:
-                return {
-                    ...state,
-                    uploader: payload
-                }
-            case SET_ERROR:
-                return {
-                    ...state,
-                    error: payload
-                }
-            case SET_COMPLETED:
-                return {
-                    ...state,
-                    completed: payload
-                }
-            default:
-                return {
-                    ...state
-                }
-        }
-    }
-
-    const [register, data] = useMutation(REGISTER, {
+    const [register] = useMutation(REGISTER, {
         onCompleted: (data) => {
-            dispatch({
-                type: SET_COMPLETED,
-                payload: true
-            });
             context.dispatch({
                 type: SET_USER,
                 payload: data.register
             });
-        },
-        onError: (error) => {
-            dispatch({
-                type: SET_ERROR,
-                payload: errorParser(error)
-            })
         }
     });
-
-    const updateUploader = (data: any) => {
-        dispatch({
-            type: UPDATE_UPLOADER,
-            payload: data
-        })
-    }
-
-    const [{inputs, uploader, error, completed}, dispatch] = useReducer(reducer, {
-        inputs: {
-            email: {
-                value: "",
-                label: "Email",
-                type: "email"
-            },
-            password: {
-                value: "",
-                label: "Password",
-                type: "password"
-            },
-            display: {
-                value: "",
-                label: "Display",
-                type: "text"
-            },
-            bio: {
-                value: "",
-                label: "Bio",
-                type: "text"
-            }
-        }
-    });
-
-
-    const submit = () => {
-        const {email, password, display, bio} = inputs;
-        register({
-            variables: {
-                ...uploader,
-                email: email.value,
-                password: password.value,
-                display: display.value,
-                bio: bio.value
-            }
-        })
-    }
-
-    const formInputs: FormInput[] = Object.keys(inputs).map(input => {
-        return {
-            ...inputs[input],
-            dispatch: ({target}) => {
-                dispatch({
-                    type: SET_INPUT,
-                    payload: [input, target.value]
-                })
-            },
-            key: input
-        }
-    });
-
-    if (completed) {
-        return <Redirect to="/me"/>
-    }
 
     return <>
         <h1>Register</h1>
-        <Form inputs={formInputs} error={error}>
-            <PhotoUploader updateState={updateUploader}/>
-        </Form>
-        <button onClick={submit} className={`${buttons.standard} ${standard.center}`}>Register!</button>
+        <ProfileForm save={ register } />
     </>
 }
 
---
+++ src/widgets/ProfileForm.tsx
@@ -0,0 +1,141 @@
+import { FetchResult } from "@apollo/client";
+import { useTheme } from "@material-ui/core";
+import { useReducer } from "react";
+import { Redirect } from "react-router-dom";
+import errorParser from "../helpers/errorParser";
+import useButtons from "../styles/buttons";
+import useStandard from "../styles/standard";
+import Form, { FormInput } from "./Form";
+import PhotoUploader from "./PhotoUploader";
+
+type ProfileFormProps = {
+    save: (data: any) => Promise<FetchResult<any, Record<string, any>, Record<string, any>>>;
+    data?: {
+        email: string;
+        display: string;
+        bio: string;
+    }
+};
+
+const ProfileForm: React.FC<ProfileFormProps> = ({save, data}) => {
+    const SET_INPUT = 'SET_INPUT';
+    const UPDATE_UPLOADER = 'UPDATE_UPLOADER';
+    const SET_ERROR = 'SET_ERROR';
+    const SET_COMPLETED = 'SET_COMPLETED';
+
+    const theme = useTheme();
+    const buttons = useButtons(theme)();
+    const standard = useStandard(theme);
+
+    const reducer = (state: any, {type, payload}: {type: string, payload: any}) => {
+        switch(type){
+            case SET_INPUT:
+                const {inputs} = state;
+                inputs[payload[0]].value = payload[1];
+                return {
+                    ...state,
+                    inputs
+                }
+            case UPDATE_UPLOADER:
+                return {
+                    ...state,
+                    uploader: payload
+                }
+            case SET_ERROR:
+                return {
+                    ...state,
+                    error: payload
+                }
+            case SET_COMPLETED:
+                return {
+                    ...state,
+                    completed: payload
+                }
+            default:
+                return {
+                    ...state
+                }
+        }
+    }
+
+    const updateUploader = (data: any) => {
+        dispatch({
+            type: UPDATE_UPLOADER,
+            payload: data
+        })
+    }
+
+    const [{inputs, uploader, error, completed}, dispatch] = useReducer(reducer, {
+        inputs: {
+            email: {
+                value: data?.email,
+                label: "Email",
+                type: "email"
+            },
+            password: {
+                value: "",
+                label: "Password",
+                type: "password"
+            },
+            display: {
+                value: data?.display,
+                label: "Display",
+                type: "text"
+            },
+            bio: {
+                value: data?.bio,
+                label: "Bio",
+                type: "text"
+            }
+        }
+    });
+
+
+    const submit = () => {
+        const {email, password, display, bio} = inputs;
+        save({
+            variables: {
+                ...uploader,
+                email: email.value,
+                password: password.value,
+                display: display.value,
+                bio: bio.value
+            }
+        })
+        .then(() => dispatch({
+                type: SET_COMPLETED,
+                payload: true
+            }))
+        .catch((error) => 
+            dispatch({
+                type: SET_ERROR,
+                payload: errorParser(error)
+            }));
+    }
+
+    const formInputs: FormInput[] = Object.keys(inputs).map(input => {
+        return {
+            ...inputs[input],
+            dispatch: ({target}) => {
+                dispatch({
+                    type: SET_INPUT,
+                    payload: [input, target.value]
+                })
+            },
+            key: input
+        }
+    });
+
+    if (completed) {
+        return <Redirect to="/me"/>
+    }
+
+    return <>
+        <Form inputs={formInputs} error={error}>
+            <PhotoUploader updateState={updateUploader}/>
+        </Form>
+        <button onClick={submit} className={`${buttons.standard} ${standard.center}`}>Register!</button>
+    </>
+};
+
+export default ProfileForm;
